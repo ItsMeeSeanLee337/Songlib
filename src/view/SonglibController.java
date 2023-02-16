8a8e4d6b37ebdbfb7e31b69146e44a635b4c9f54
Sean Lee <slee206061@gmail.com>
2023-02-15 19:21:04 -0500

    Files for Songlib

---
+++ src/view/SonglibController.java
@@ -0,0 +1,50 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class SonglibController {
+
+    @FXML
+    private Button AddSongButton;
+
+    @FXML
+    private TextField AlbumTextf;
+
+    @FXML
+    private TextField ArtistTextf;
+
+    @FXML
+    private Button DeleteSongButton;
+
+    @FXML
+    private Button EditSongButton;
+
+    @FXML
+    private ListView<?> ListOfSongs;
+
+    @FXML
+    private TextField SongNameTextf;
+
+    @FXML
+    private TextField YearTextf;
+
+    @FXML
+    void AddSongClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteSongClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditSongClicked(ActionEvent event) {
+
+    }
+
+}
