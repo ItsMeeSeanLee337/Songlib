bd0e3d3b6e0823a96b79a82681195e601a254d95
ImFromNewJersey <caguliar@ymail.com>
2023-02-18 17:09:28 -0500

    Addition of pop up confirmations for Add and Delete

--- src/view/SonglibController.java
+++ src/view/SonglibController.java
@@ -1,10 +1,15 @@
 package view;
 
+import java.util.Optional;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class SonglibController {
@@ -41,20 +46,47 @@ public class SonglibController {
 
     @FXML
     void AddSongClicked(ActionEvent event) {
+        //sets the name of each category in the textbox
         String name = SongNameTextf.getText().trim();
         String artist = ArtistTextf.getText().trim();
         String album = AlbumTextf.getText().trim();
         String year = YearTextf.getText().trim();
+        //combines each category int one string newSong
         String newSong = name + " - " + artist + " (" + album + ", " + year + ")";
-        ListOfSongs.getItems().add(newSong);
+
+        //creates a new alert, sets the title and text, and then stores the resulting button press in confirmation result
+        Alert confirmation = new Alert(AlertType.CONFIRMATION);
+        confirmation.setTitle("Add");
+        confirmation.setContentText("Are you sure you want to add this song?");
+        Optional<ButtonType> confirmationresult = confirmation.showAndWait();
+        
+        //if the confirmation result is ok, add
+        if (confirmationresult.get() == ButtonType.OK){
+            ListOfSongs.getItems().add(newSong);
+        }
+
     }
 
     @FXML
     void DeleteSongClicked(ActionEvent event) {
+
+        //selected index is the currently selected index
         int selectedIndex = ListOfSongs.getSelectionModel().getSelectedIndex();
-        if (selectedIndex >= 0) {
-            ListOfSongs.getItems().remove(selectedIndex);
+
+        //creates a new alert, sets the title and text, and then stores the resulting button press in confirmation result
+        Alert confirmation = new Alert(AlertType.CONFIRMATION);
+        confirmation.setTitle("Delete");
+        confirmation.setContentText("Are you sure you want to delete this song?");
+        Optional<ButtonType> confirmationresult = confirmation.showAndWait();
+
+        //if the confirmation result is OK, delete
+        if(confirmationresult.get() == ButtonType.OK){
+            if (selectedIndex >= 0) {
+                //removes the selected song from the index
+                ListOfSongs.getItems().remove(selectedIndex);
+            }
         }
+
     }
 
 
